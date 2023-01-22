1a9b538aa90fae373bb430fef25ad06f178a7aec
Fernando Rodrigues <fernando@ezoom.com.br>
2023-01-22 18:44:28 -0300

    update wallet after success checkout

--- src/screens/home/home.repository.ts
+++ src/screens/home/home.repository.ts
@@ -1,12 +1,17 @@
 import { Maybe, NftData } from "../../graphql/types"
 import { NFTData } from "../../system/interfaces/common.interfaces"
+import { generateFakeCategory } from "../../system/utils/generate-fake-categories.utils";
 
 export const nftDataList = (nftList: Maybe<Array<Maybe<NftData>>>): NFTData[] => {
   if (!nftList || !nftList?.length) {
     return [] as NFTData[];
   }
   let nfts = nftList.map((nft) => {
-    return nft;
+    const category = generateFakeCategory();
+    return {
+      ...nft,
+      category
+    };
   });
 
   return nfts as NFTData[];
