dba522a458cb6baa91e834ac2e8d867f60f653a6
Fernando Rodrigues <fernando@ezoom.com.br>
2023-01-22 21:09:38 -0300

    removing fakeCategory generator

--- src/screens/home/home.repository.ts
+++ src/screens/home/home.repository.ts
@@ -1,17 +1,12 @@
 import { Maybe, NftData } from "../../graphql/types"
 import { NFTData } from "../../system/interfaces/common.interfaces"
-import { generateFakeCategory } from "../../system/utils/generate-fake-categories.utils";
 
 export const nftDataList = (nftList: Maybe<Array<Maybe<NftData>>>): NFTData[] => {
   if (!nftList || !nftList?.length) {
     return [] as NFTData[];
   }
   let nfts = nftList.map((nft) => {
-    const category = generateFakeCategory();
-    return {
-      ...nft,
-      category
-    };
+    return nft;
   });
 
   return nfts as NFTData[];
